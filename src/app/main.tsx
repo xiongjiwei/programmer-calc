495b820d4f64975776a8f002c548012eb588467b
xiongjiwei <xiongjiwei1996@outlook.com>
2024-01-03 21:52:52 +0800

    init

---
+++ src/app/main.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@nextui-org/react";
+
+export default function Home() {
+    return (
+        <div className='flex flex-col justify-center items-center w-9/12'>
+            <div>
+                显示
+            </div>
+            <button className="bg-red-500">
+                Button
+            </button>
+        </div>
+    )
+}
