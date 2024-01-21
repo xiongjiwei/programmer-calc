6b87c135fca75cc66d37c40d7fe7a6385fd3577b
xiongjiwei <xiongjiwei1996@outlook.com>
2024-01-21 17:23:50 +0800

    deploy on github pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const repo = 'programmer-calc'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
+const nextConfig = {
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+    output: "export",
+}
 
 module.exports = nextConfig
